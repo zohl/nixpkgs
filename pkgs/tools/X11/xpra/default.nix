5dfc4115c1bc4448d11dd60e17f120385fd43e98
Aristid Breitkreuz <aristidb@gmail.com>
2014-10-08 19:38:19 +0200

    xpra: update from 0.11.6 to 0.14.8

--- pkgs/tools/X11/xpra/default.nix
+++ pkgs/tools/X11/xpra/default.nix
@@ -4,12 +4,12 @@
 , ffmpeg, x264, libvpx, pil, libwebp }:
 
 buildPythonPackage rec {
-  name = "xpra-0.11.6";
+  name = "xpra-0.14.8";
   namePrefix = "";
 
   src = fetchurl {
     url = "http://xpra.org/src/${name}.tar.bz2";
-    sha256 = "0n3lr8nrfmrll83lgi1nzalng902wv0dcmcyx4awnman848dxij0";
+    sha256 = "13cy221i7yxmbw0a1h6r96l5has6dzmi3yqby9hdgfxq39lzm6mc";
   };
 
   buildInputs = [
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     xorg.libX11 xorg.renderproto xorg.libXrender xorg.libXi xorg.inputproto xorg.kbproto
     xorg.randrproto xorg.damageproto xorg.compositeproto xorg.xextproto xorg.recordproto
     xorg.xproto xorg.fixesproto xorg.libXtst xorg.libXfixes xorg.libXcomposite xorg.libXdamage
-    xorg.libXrandr
+    xorg.libXrandr xorg.libxkbfile
 
     pango cairo gdk_pixbuf atk gtk glib
 
@@ -36,7 +36,7 @@ buildPythonPackage rec {
   preBuild = ''
     export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE $(pkg-config --cflags gtk+-2.0) $(pkg-config --cflags pygtk-2.0) $(pkg-config --cflags xtst)"
   '';
-  setupPyBuildFlags = ["--enable-Xdummy"];
+  setupPyBuildFlags = ["--with-Xdummy"];
 
   preInstall = ''
     # see https://bitbucket.org/pypa/setuptools/issue/130/install_data-doesnt-respect-prefix
